b2782242fab6bdd0742f3532acbfb7ba534e99ff
cosmicnotepin <shardt@posteo.net>
2020-07-08 13:43:21 +0200

    cleanupData() to sanitize training data(no wim data, no pictures)

--- parsePlot.py
+++ parsePlot.py
@@ -6,12 +6,14 @@ import os
 import sys
 from pandas import *
 from datetime import datetime
+import contextlib
 
 #textitext
 maxAxles = 10
 #directory = r'C:\Users\borgro\svn\java\SensorFusion\Daten\Daten'
-directory = r'D:\Rohdaten\FRV\Daten'
+#directory = r'D:\Rohdaten\FRV\Daten'
 #directory = r'D:\Rohdaten\FREII1\Daten'
+directory = r'D:\Rohdaten\FRV_FREII1clean\Daten'
 class2Marker = { "nicht_klass_Kfz":dict(marker='x', c='black'), "Motorrad":dict(marker='4', c='yellow'), "PKW":dict(marker='o', c='blue'), "Kleintransporter":dict(marker='>', c='green'), "PKW_Anhaenger":dict(marker='s', c='cyan'), "LKW":dict(marker='+', c='red'), "LKW_Anhaenger":dict(marker='P', c='magenta'), "Sattel_Kfz":dict(marker='*', c='brown'), "Bus":dict(marker='_', c='DarkGoldenRod'), "ungueltig":dict(marker='None', c='DarkGoldenRod')  }
 
 
@@ -169,4 +171,45 @@ def explainPKWVsKleintransporter():
 
 def fixData():
     vehicles = parseVehicleFiles()
+
+def cleanupData():
+    directory = r'D:\Rohdaten\FRV_FREII1clean'
+    vehicles = []
+    noVDel = 0
+    noPDel = 0
+    #'\d to differentiate from wim.txt files
+    for entry in filter(lambda x: re.search(r'\d.txt', x.path), os.scandir(directory + r'\Daten')):
+        delete = False
+        with open(entry.path, 'r') as vf:
+            line = vf.readline()
+            pattern = '.*? length=(?P<length>.*?) weight=(?P<weight>.*?) maxAxleWeight=(?P<maweight>.*?) axles=(?P<axles>.*?) axleWeights=(?P<axleWeights>.*?) .*? numWimFiles=(?P<wim>[0-9]+) numAnprPics=(?P<anpr>[0-9]+) numIpCamPics=(?P<cam>[0-9]+) numScannerPics=(?P<scanner>[0-9]+)'
+            match = re.match(pattern, line)
+
+            #no wim data or no pictures means it is useless
+            if(match.group('wim') == '0' or match.group('scanner') == '0'): 
+                delete = True
+                with contextlib.suppress(FileNotFoundError):
+                    os.remove(entry.path.replace(".txt", ".cls")) 
+                    vf.readline() #skip empty line
+                    pattern = r'.*?(?P<relPath>\\Daten.*)'
+                    for i in range(0, int(match.group('wim'))):
+                        line = vf.readline()
+                        wimMatch = re.match(pattern, line)
+                        print(entry)
+                        os.remove(directory + wimMatch.group('relPath'))
+                        noPDel += 1
+                    noPics = int(match.group('anpr')) + int(match.group('cam')) + int(match.group('scanner')) 
+                    pattern = r'.*?(?P<relPath>\\Bilder.*)'
+                    for i in range(0, noPics):
+                        line = vf.readline()
+                        picMatch = re.match(pattern, line)
+                        os.remove(directory + picMatch.group('relPath'))
+                        noPDel += 1
+
+        if (delete):
+            os.remove(entry)
+            noVDel += 1
+
+    print('removed vehicles: ' + str(noVDel) + ' removed pictures: ' + str(noPDel))
+
 
