8d97a8533db2c664c1522f7b9e1768d7a378e74d
cosmicnotepin <shardt@posteo.net>
2020-08-28 10:37:22 +0200

    classify into two classes only with mapper-functions as parameter to parsing function, parsing and plotting of scanObjs

---
+++ plotScanObjs.py
@@ -0,0 +1,40 @@
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.art3d as art3d
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+import parse
+
+def plotDebugLog(path=r'D:\Rohdaten\FRV_FREII1clean\Logs\2020-05-25_00-00-00_NKP-FREII1-debug.log'):
+    scanObjs = parse.parseDebugLog(path)
+    request = int(input('id to display: '))
+    while(request != 0):
+        vhcl = scanObjs[request]
+        plotVehicle(vhcl)
+        request = int(input('id to display: '))
+
+def plotVehicle(vhcl):
+    vhcl.sort(key=lambda x: x['scantime'], reverse=True)
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.set_ylim(bottom=0, top=30)
+    ax.set_zlim(bottom=0, top=30)
+    ax.set_xlim(left=0, right=30)
+    for scanObj in vhcl:
+        rect = Rectangle([0, scanObj['min_y']], scanObj['width'], scanObj['height'], fill=False)
+        ax.add_patch(rect)
+        #print(scanObj['scantime'])
+        #print(vhcl[0]['scantime'])
+        #print(scanObj['scantime'] - vhcl[0]['scantime'])
+        art3d.pathpatch_2d_to_3d(rect, z=-1*(scanObj['scantime'] - vhcl[0]['scantime'])*11, zdir="x") # 11 ~ 40km/h in m/s
+    #ax.relim()
+    #ax.autoscale_view()
+    plt.show()
+    #plt.draw()
+    #plt.pause(0.001)
+    #ax.autoscale(tight=True)
+
+plotDebugLog()
